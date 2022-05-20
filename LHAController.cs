ea184d2f3bd232cad1e6d4994db405815d4bd210
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2022-05-20 16:19:19 -0400

    optimize

--- LHAController.cs
+++ LHAController.cs
@@ -1,88 +1,67 @@
 using Comfort.Common;
 using EFT;
-using System.Collections;
 using System.IO;
 using System.Media;
 using UnityEngine;
 
-namespace LHA
+namespace LHA // LHA sounds like an alternative to LUA or something
 {
-    public class LHAController : MonoBehaviour
+    public class LHAController : MonoBehaviour // controller script, the brains of the mod that runs in Mono
     {
         private SoundPlayer soundPlayer;
-        private bool isLoaded = false;
-        private bool trigger = false;
-        private SessionResultPanel sessionResultPanel;
+        private bool isPlaying = false; // I have to use this bool, turns out attempting to load and play audio every frame doesn't end well
 
-        public void Update()
+        public void Update() // base unity mono method that runs every frame
         {
-            var gameWorld = Singleton<GameWorld>.Instance;
-            if (gameWorld.AllPlayers == null || gameWorld.AllPlayers.Count <= 0 || gameWorld.AllPlayers[0] is HideoutPlayer)
+            var r = Ready(); // check if stuff is loaded
+            var t = Trigger(); // check if threshold is met
+
+            if (r == false)
                 return;
 
-            sessionResultPanel = Singleton<SessionResultPanel>.Instance;
-            if (sessionResultPanel != null)
+            if (t && !isPlaying) // if threshold is met and alert isn't already plating
             {
-                trigger = false
-                return;
+                isPlaying = true;
+                soundPlayer.PlayLooping(); // play alert
+            } else // threshold not met?
+            {
+                isPlaying = false;
+                soundPlayer.Stop(); // end alert
             }
+        }
 
-            if (!isLoaded)
-                LoadAudio();
-
-            var current = gameWorld.AllPlayers[0].HealthController.GetBodyPartHealth(EBodyPart.Common).Current;
-
-            if (current > 220)
+        bool Ready() // checks if script is ready to run without vomiting errors
+        {
+            var gameWorld = Singleton<GameWorld>.Instance; // checks if gameworld is instantiated, use singleton so that there's only one instance per scene
+            var sessionResultPanel = Singleton<SessionResultPanel>.Instance; // checks result panel, good end point because it's instantiated immediately after death or extract
+            if (gameWorld.AllPlayers == null || gameWorld.AllPlayers.Count <= 0 || gameWorld.AllPlayers[0] is HideoutPlayer || sessionResultPanel != null) // if important stuff isn't loaded
             {
-                if (!trigger)
-                    return;
-
-                trigger = false;
+                return false; // return false
             }
-            else
-                trigger = true;
+            // if important stuff is loaded
+            return true; // return true
         }
 
-        public IEnumerator Start()
+        bool Trigger() // checks player health to decide whether to play the alert or not
         {
-            StartCoroutine(Check());
-            yield break;
-        }
+            var gameWorld = Singleton<GameWorld>.Instance; // gaemwordl
 
-        private IEnumerator Check()
-        {
-            yield return new WaitUntil(() => trigger == true);
-            OnEvent();
-            yield return new WaitUntil(() => trigger == false);
-            OnEventEndOrNull();
-            StartCoroutine(Check());
-            yield break;
-        }
+            var current = gameWorld.AllPlayers[0].HealthController.GetBodyPartHealth(EBodyPart.Common).Current; // get player's current total health
 
-        private void OnEvent()
-        {
-            soundPlayer.PlayLooping();
-        }
-
-        private void OnEventEndOrNull()
-        {
-            soundPlayer.Stop();
-        }
-
-        private void LoadAudio()
-        {
-            if (isLoaded)
-                return;
-
-            var fullPath = Directory.GetCurrentDirectory() + "\\BepInEx\\plugins\\LHA\\alert.wav";
-
-            soundPlayer = new SoundPlayer
+            if (current > 220) // if health is above 220
             {
-                SoundLocation = fullPath
-            };
+                return false; // return false, do not allow for alert
+            } else // if player is below 220
+            {
+                return true; // return true, allow for alert alert
+            }
+        }
 
-            soundPlayer.Load();
-            isLoaded = true;
+        public void LoadAudio(string fullpath) // self explainatory
+        {
+            soundPlayer.SoundLocation = Directory.GetCurrentDirectory() + fullpath; // fetch audio file
+
+            soundPlayer.Load(); // load audio file
         }
     }
 }
